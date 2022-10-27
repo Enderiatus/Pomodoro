3f1fc3560b05913e1a7f43ae74df19e81bab8d3f
Enderiatus <ender.ordu.1905@gmail.com>
2022-10-28 01:24:30 +0300

    bug fix and optimization

--- scripts/script.js
+++ scripts/script.js
@@ -1,82 +1,47 @@
 'use strict'
 
-let progress = document.getElementById('progress')
-let dot = document.getElementsByClassName('circle-dot')[0]
-let timer = document.getElementById('timer-span')
-let popup = document.getElementsByClassName('settings')[0]
+let progress, dot, timerSpan;
+let popup;
 
-let startButton = document.getElementById('start-button')
-let stopButton = document.getElementById('stop-button')
+let startButton, stopButton;
 
-let workingSelection = document.getElementById('working')
-let restSelection = document.getElementById('rest')
+let workingSelection, restSelection;
 
-let work_time = 0;
-let rest_time = 0;
-
-let secTotal = 0;
-let secLeft = 0;
-let dateFull = 0;
-let timerInterval = null;
-
-let running = false;
-let pomotype = null;
+let currentTime, dateFull, timerInterval, pomoType, times, running;
 
 let sound = new Audio("sounds/alarm-sound.wav")
 
+/* functions for buttons */
+
 function _timer(callback) {
+    if(!sound.paused) sound.pause();
+    console.log(running)
     if(callback == "start") {
-        sound.pause()
-        if(!secTotal > 0) 
-            return;
-        if (!running) {
-            running = true
-            /*
-            if(secTotal == 0) {
-                secTotal = new Date(document.getElementById('timer-span').textContent.split(':')[0]*60*1000+document.getElementById('timer-span').textContent.split(':')[1]*1000).getTime()/1000
-                secLeft = 0
-                dateFull = new Date(secTotal*1000);
-            } */
-            toggleButtons();
-            changeWorking();
-            timerInterval = setInterval(() => {
-                secLeft += 1
-                dateFull.setSeconds(dateFull.getSeconds()-1)
-                document.getElementById('timer-span').textContent = dateFull.toISOString().substr(14, 5);
-                progress.style.strokeDashoffset = 930 - ((secLeft/secTotal)*9.3)*100
-                //console.log(secLeft/secTotal*3.6*100)
-                //console.log(930 - ((secLeft/secTotal)*9.3)*100)
-                dot.style.transform = `rotate(${secLeft/secTotal*3.6*100}deg`
-                if(secLeft >= secTotal) {
-                    clearInterval(timerInterval)
-                    if(pomotype) {
-                        secTotal = rest_time*60;
-                        dateFull = new Date(secTotal*1000);
-                        secLeft = 0;
-                        document.getElementById('timer-span').textContent = dateFull.toISOString().substr(14, 5);
-                        dot.style.transform = `rotate(0deg)`
-                        progress.style.strokeDashoffset = 930;
-                        timerInterval = null;
-                        changeWorking();
-                        toggleButtons();
-                        pomotype = false;
-                    }else {
-                        loadSettings();
-                        changeWorking();
-                        toggleButtons();
-                    }
-                    running = false;
-                    sound.play()
-                }
-            }, 10);
-        }
+        changeStatus();
+        running = true;
+        timerInterval = setInterval(timerProgress, 10);
     }else if(callback == "stop") {
-        clearInterval(timerInterval)
-        changeWorking();
-        toggleButtons();
+        toggleButton();
         running = false;
+        clearInterval(timerInterval);
     }
-    
+}
+
+/* after save click */
+
+function loadSettings() {
+    times['pomo'] = parseInt(document.getElementById('work-time').value)*60;
+    times['rest'] = parseInt(document.getElementById('break-time').value)*60;
+    if(times['pomo'] && times['rest']) {
+        if(times['pomo'] < 3600 && times['rest'] < 3600) {
+            pomoType = 'pomo'
+            circleReset();
+            if(running) 
+                changeStatus();
+                running = false;
+
+        }
+    }
 }
 
 function toggleSettings() {
@@ -88,36 +53,67 @@ function applySettings() {
     toggleSettings();
 }
 
+/* timer for circle */ 
 
-function loadSettings() {
-    work_time = document.getElementById('work-time').value;
-    rest_time = document.getElementById('break-time').value;
-    if(work_time && rest_time) {
-        if(work_time < 60 && rest_time < 60) {
-            secTotal = work_time*60;
-            dateFull = new Date(secTotal*1000);
-            secLeft = 0;
-            document.getElementById('timer-span').textContent = dateFull.toISOString().substr(14, 5);
-            dot.style.transform = `rotate(0deg)`
-            progress.style.strokeDashoffset = 930;
-            timerInterval = null;
-            pomotype = true
-        }
+function timerProgress() {
+    currentTime += 1
+    dateFull.setSeconds(dateFull.getSeconds()-1)
+    timerSpan.textContent = dateFull.toISOString().substr(14, 5);
+    progress.style.strokeDashoffset = 930 - ((currentTime/times[pomoType])*9.3)*100
+    dot.style.transform = `rotate(${currentTime/times[pomoType]*3.6*100}deg`
+    if(currentTime >= times[pomoType]) {
+        clearInterval(timerInterval)
+        pomoType = pomoType == 'pomo' ? pomoType = 'rest' : pomoType='pomo';
+        circleReset();
+        changeStatus();
+        running=false;
+        //sound.play()
     }
 }
 
-function toggleButtons() {
-    startButton.classList.toggle('active-btn');
-    stopButton.classList.toggle('active-btn');
+/* resetting circle progress and times */
+
+function circleReset() {
+    clearInterval(timerInterval)
+    currentTime = 0;
+    dateFull = new Date(times[pomoType]*1000);
+    timerSpan.textContent = dateFull.toISOString().substr(14, 5);
+    progress.style.strokeDashoffset = 930
+    dot.style.transform = `rotate(0deg)`
 }
 
-function changeWorking() {
-    if(pomotype) {
+/* start/stop button and status bar */
+
+function changeStatus() {
+    if(pomoType == 'pomo') {
         workingSelection.classList.add('active')
         restSelection.classList.remove('active')
     }else {
         workingSelection.classList.remove('active')
         restSelection.classList.add('active')
     }
-    
+    toggleButton()
 }
+
+function toggleButton() {
+    startButton.classList.toggle('active-btn');
+    stopButton.classList.toggle('active-btn');
+}
+
+/* loading default values */
+
+function loadVariables() {
+    progress = document.getElementById('progress')
+    dot = document.getElementsByClassName('circle-dot')[0]
+    timerSpan = document.getElementById('timer-span')
+    popup = document.getElementsByClassName('settings')[0]
+    startButton = document.getElementById('start-button')
+    stopButton = document.getElementById('stop-button')
+    workingSelection = document.getElementById('working')
+    restSelection = document.getElementById('rest')
+    pomoType = 'pomo'
+    times = {pomo:1500, rest:300};
+    circleReset(); 
+}
+
+loadVariables();
